e8b5b1ebdd68b27f36a391eb2244485abb4e5ea7
AltinKrasniqi <altinkrasniqi427@gmail.com>
2023-12-15 16:53:38 -0500

    Added UserBioView

---
+++ src/lib/client/component/userBioView.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    import UserIcon from "$lib/client/component/userIcon.svelte";
+    import type { ClientUser } from "../objects/user";
+
+    export let data: ClientUser;
+</script>
+
+<div class="left-hand-side">
+    <div class="icon">
+        <UserIcon user={data}/>
+    </div>
+    <div class="names">
+        <pre class="name">{data.username}</pre>
+        <pre class="handle">@{data.handle}</pre>
+        <pre class="bio">{data.bio}</pre>
+    </div>
+</div>
+
+<style>
+
+    .icon {
+        padding-bottom:30px;
+        width: 50px;
+        height: 50px;
+    }
+
+    .names{
+        color: white;
+    }
+
+    .handle {
+        font-size: 12px;
+        margin: 0;
+        color: var(--c-zinc-500);
+    }
+
+    .name{
+        margin: 0;
+    }
+    .bio{
+        font-size:12px;
+
+    }
+
+    .left-hand-side {
+        margin: 0;
+        border: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+        align-items: center;
+    }
+    
+</style>
