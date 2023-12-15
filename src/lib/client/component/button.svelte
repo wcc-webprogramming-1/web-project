7fa663c2cf660202e46e0a6185bebcbddbf99b37
AltinKrasniqi <altinkrasniqi427@gmail.com>
2023-12-15 11:21:21 -0500

    Added followers/following component

--- src/lib/client/component/button.svelte
+++ src/lib/client/component/button.svelte
@@ -1,11 +1,11 @@
 <script lang='ts'>
-
+    export let contents: string;
 </script>
 
 <div>
-    <button>
-    </button>
+    <button>{contents}</button>
 </div>
+
 
 <style>
     button{
@@ -14,5 +14,8 @@
         border: 1px solid grey;
         border-radius: 25px;
         background-color: black;
+        color:white;
+        font-size:14px;
+        font-weight:600;
     }
 </style>
--- src/routes/u/[user_handle]/+page.svelte
+++ src/routes/u/[user_handle]/+page.svelte
@@ -3,9 +3,16 @@
   import UserBanner from '$lib/client/component/userBanner.svelte';
   import UserIcon from '$lib/client/component/userIcon.svelte';
   import { base } from "$app/paths";
+  import Button from '$lib/client/component/button.svelte';
+  import CalendarIcon from '$lib/client/component/icon/calendarIcon.svelte';
   import type * as Type from './$types'
+  import { Session } from '$lib/client/stores/session';
+  import FollowsCount from '$lib/client/component/followsCount.svelte';
 
   export let data: Type.PageData;
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];4
+
+  $: is_self = $Session.isLoggedIn && $Session.user.id == data.self.id;
 </script>
 
 <Header title={data.self.username} description="This is where the post count will go" back_path="{base}/" />
@@ -15,13 +22,19 @@
     <UserBanner />
     <div class="icon">
       <UserIcon user={data.self} />
+      <div class = "followbutton"><Button contents={is_self ? "Edit Profile" : "Follow"}/></div>
     </div>
   </div>
 
   <div class="content">
     <h1>{data.self.username}</h1>
     <h2>@{data.self.handle}</h2>
-    <p>{data.self.bio}</p>
+    <p class="bio">{data.self.bio}</p>
+    <p class = "joindate"><CalendarIcon size={20} color="gray-500"/>Joined {months[data.self.creationDate.getMonth()]} {data.self.creationDate.getFullYear()}</p>
+    <p class="followsCount"><FollowsCount
+      followers_count={data.self.followerCount}
+      following_count={data.self.followingCount}  
+    /></p>
   </div>
 </div>
 
@@ -46,31 +59,50 @@
     outline: 4px solid black;
     border-radius: 999px;
   }
+  
+  .followbutton{
+    display:flex;
+    position:relative;
+    left:425px;
+    bottom:65px;
+  }
 
   .content {
     margin-left: 25px;
-    margin-top: 25px;
+    margin-top: 15px;
   }
 
   .content > h1 {
-    font-size: 32px;
-    font-weight: 400;
+    font-size: 22px;
+    font-weight: 700;
     color: var(--c-white);
     margin: 0;
     line-height: 32px;
   }
 
   .content > h2 {
-    font-size: 16px;
-    font-weight: 600;
+    font-size: 14px;
+    font-weight: 500;
     color: var(--c-zinc-500);
     margin: 0;
   }
 
-  .content > p {
-    font-size: 18px;
+  .bio {
+    color:white;
+    font-size: 15px;
     font-weight: 400;
-    color: var(--c-white);
-    margin-top: 24px;
+    margin-top:12px;
+  }
+
+  .joindate {
+    font-size: 15px;
+    font-weight: 400;
+    color:grey;
+    margin-top: 15px;
+    display:flex;
+    gap:3px;
+  }
+  .followsCount{
+    font-size:15px;
   }
 </style>
